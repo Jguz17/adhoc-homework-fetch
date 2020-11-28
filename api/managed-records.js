67717c90493993433058cd25e9c4641605f6d7af
jguz17 <johnathanguzman17@gmail.com>
2020-11-28 09:47:07 -0600

    bare bones

--- api/managed-records.js
+++ api/managed-records.js
@@ -1,9 +1,26 @@
-import fetch from "../util/fetch-fill";
+import fetch from "../util/fetch-fill.js";
 import URI from "urijs";
 
 // /records endpoint
-window.path = "http://localhost:3000/records";
+// window.path = "http://localhost:3000/records";
+
+const API = 'http://localhost:3000/records'
 
 // Your retrieve function plus any additional functions go here ...
+const retrieve = (options) => {
+    if (!options) {
+        fetch(`${URI(API).addSearch('limit', '10')}`)
+        .then((res) => {
+            if (res.status !== 200) {
+                console.log('Error')
+            } else {
+                res.json()
+                .then((data) => console.log(data))
+            }
+        })
+    }
+}
+
+retrieve()
 
 export default retrieve;
